170309811aa2b0d326bc6df58b980226422c6aaf
nilesh <nillchauhan00@gmail.com>
2024-08-18 10:31:47 +0545

    mysql commit

---
+++ mysql/project.sql
@@ -0,0 +1,11 @@
+-- create table client_master(client_no int, -- name varchar(20), city varchar(20), pincode int, state varchar(10), baldue int); 
+
+-- insert into client_mater values(101,'milan','surat',394210,'gujarat',5500);
+
+create table product_master(product_no int, desc_ription varchar(20), profit_percent int, unit_measure varchar(10), qtyon_hand int, reorder_lv1 int, sell_price int, cost_price int);
+
+insert into product_master values(00001,'shirt',5,piece,200,50,350,250)
+
+create table salesman_master(salesman_no int, name varchar(20), city varchar(20), pincode int, state varchar(20));
+
+insert into salesman_master values(00001,'aman','mumbai',400002,'gujarat');
